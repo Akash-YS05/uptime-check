fa8c392dc8d77c2fad8f3b664991b45b8d781a25
Akash-YS05 <akash.pan05@gmail.com>
2025-05-21 09:03:48 +0530

    adding website logic

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,9 +1,24 @@
 import express, { type Request, type Response } from "express";
 import { authMiddleware } from "./middleware";
 const app = express();
+import { prismaClient } from "db/client";
 
+app.use(express.json())
 
-app.post("/api/v1/website", authMiddleware, (req: Request, res: Response) => {
+app.post("/api/v1/website", authMiddleware, async (req: Request, res: Response) => {
+    const userId = req.userId!;
+    const { url } = req.body;
+
+    const data = await prismaClient.websites.create({
+        data:{
+            userId,
+            url
+        }
+    })
+
+    res.json({
+        id: data.id
+    })
 
 })
 
